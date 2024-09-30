2385f59d68868359ad564f9d64eee90838b5026e
Dim0nMC <108579052+Dim0nMC@users.noreply.github.com>
2024-10-01 01:35:26 +0300

    Update taylor.fsx

--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,41 @@
-// Print a table of a given function f, computed by taylor series
+open System
 
-// function to compute
-let f = sin
+let builtInFunc x = exp(2.0 * x)
 
-let a = 0.0
-let b = 1.0
+let a = 0.1
+let b = 0.6
 let n = 10
+let eps = 0.000000001
 
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
+let rec fact n =
+    if n <= 1 then 1.0
+    else float n * fact (n - 1)
 
 
-// Define a function to do the same in a more efficient way
-let taylor = f
+let taylorNaive x =
+    let rec naive k acc term =
+        if abs(term) < eps then acc, k
+        else
+            let term = (2.0 * x) ** float k / fact k
+            naive (k + 1) (acc + term) term
+    naive 1 1.0 1.0
 
+let taylorSmart x =
+    let rec smart k acc term =
+        if abs(term) < eps then acc, k
+        else
+            let term = term * (2.0 * x) / float (k + 1)
+            smart (k + 1) (acc + term) term
+    smart 0 1.0 1.0 
+
+printfn "|   x   |   Builtin  | Smart Taylor | #terms  | Naive Taylor | #terms |"
+printfn "-----------------------------------------------------------------------"
 let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
+    for i = 0 to n do
+        let x = a + (float i) / float n * (b - a)
+        let smartVal, termsSmart = taylorSmart x
+        let naiveVal, termsNaive = taylorNaive x
+        printfn "| %5.2f |  %-10.6f|   %-10.6f |    %d   |  %-10.6f  |    %d   |" 
+            x (builtInFunc x) smartVal termsSmart naiveVal termsNaive
 
 main
-
